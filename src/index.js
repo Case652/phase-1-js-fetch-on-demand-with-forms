25b22aedf84964569848f57ee0b466b9f3830ff3
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-17 21:33:17 -0500

    fetched movies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
 
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector(`section#movieDetails h4`);
+            title.innerText = data.title;
+            const summary = document.querySelector(`section#movieDetails p`);
+            summary.innerText = data.summary;
+        });
+    });
+};
+    
+    
+    
+    
+    /*const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        console.log(event.target.children[1].value);
+        event.target.children[1].value;
+    });
+}
+*/
 document.addEventListener('DOMContentLoaded', init);
